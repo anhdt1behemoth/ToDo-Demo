5897c6001e7026e84d452e05dd3dc626813dedfb
anhdt1behemoth <anhdt@behemoth.vn>
2022-05-11 11:12:22 +0700

    TodoDemoVuex

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <AddTodos/>
+      <FilterTodo/>
+      <TodosDemo/>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodosDemo from './components/TodosDemo.vue'
+import AddTodos from './components/AddTodo.vue'
+import FilterTodo from './components/FilterTodo.vue'
+export default {
+  name: 'App',
+  components: {
+    TodosDemo,
+    AddTodos,
+    FilterTodo
+  }
+}
+</script>
+
+<style>
+
+</style>
